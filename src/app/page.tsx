df92a675fd563acbc022da1bff947a0aec09e2cd
poom-ytthpch <yutthapichai.cha@ext.axonstech.com>
2025-11-14 17:00:27 +0700

    update cv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
 
             <div className="mt-4 flex w-full justify-around">
               <a
-                href="/Yutthapichai-CV.pdf"
+                href="/YutthapichaiCV.pdf"
                 target="_blank"
                 className="flex items-center px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition"
               >
